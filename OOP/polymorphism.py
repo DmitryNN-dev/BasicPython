e4cca09fc8d13a67d4be67cf6e24fc6f0fed259d
BlackCasque <tihonovdima3307@gmail.com>
2024-02-06 20:03:36 +0300

    new_knowledge

--- OOP/polymorphism.py
+++ OOP/polymorphism.py
@@ -6,7 +6,6 @@ class Buildinig:
         self.build_name = build_name
         self.year = year
         self.city = city
-        self.get_info()
     
     def get_info(self):
         print(f"Build_name: {self.build_name}; Year: {self.year}; City: {self.city}")
@@ -23,10 +22,14 @@ class School(Buildinig):
     def __init__(self, build_name, people, year, city):
         super(School, self).__init__(build_name, year, city)
         self.people = people
+
+    def get_info(self):
+        super().get_info()
+        print("People:", self.people)
     pass
 
 class House(Buildinig):
     pass
 
 school1 = School("first_school", 150, 2000, "Moscow")
-house1 = House("House1", 2000, "Moscow")
+school1.get_info()
